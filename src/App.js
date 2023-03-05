7eadd784c96af495451029f13101313e26883a72
Jesse Abarca <JesseAbarca888@gmail.com>
2023-03-04 22:38:10 -0800

    Set up basic input component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import MealsSummary from './components/MealsSummary';
+import Input from './components/UI/Input';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MealsSummary />
+      <Input
+      label="Amount"
+      />
     </div>
   );
 }
