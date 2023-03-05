71a9c177e48609678a5e45f0e3491a5c0d8310c2
Jesse Abarca <JesseAbarca888@gmail.com>
2023-03-04 22:22:02 -0800

    Clean terminal errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,11 @@ import meals from '../assets/meals.jpg';
 const Header = () => {
     return (
         <>
-            <header className={classes.header}></header>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+            </header>
             <div className={classes["main-image"]}>
-                <img src={meals}/>
+                <img src={meals} alt=""/>
             </div>
         </>
     );
