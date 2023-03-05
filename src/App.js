d9e8eace2214df7fa3e561bc60bbddd6f4892b5a
Jesse Abarca <JesseAbarca888@gmail.com>
2023-03-04 22:45:11 -0800

    Add MealItemForm component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
     <div className="App">
       <Header />
       <MealsSummary />
-      <Input
-      label="Amount"
-      />
     </div>
   );
 }
---
+++ src/components/MealItem/MealItemForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = () => {
+    return(
+        <>
+            <form className={classes.form}>
+
+            </form>
+        </>
+    );
+}
+
+export default MealItemForm;
