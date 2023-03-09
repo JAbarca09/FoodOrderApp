426ee77e40d21864f0902ac1f3261697f1950f48
Jesse Abarca <JesseAbarca888@gmail.com>
2023-03-08 22:17:06 -0800

    Create Card component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
 
 const DUMMY_MEALS = [
   {
@@ -29,16 +30,15 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
 
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
-
-    return(
-        <section className={classes.meals}>
-            <ul>
-                {mealsList}
-            </ul>
-        </section>
-    );
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
 };
 
 export default AvailableMeals;
