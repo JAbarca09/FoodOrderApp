404d7377fe6527ab03201f4972cb93bedd4bc325
Jesse Abarca <JesseAbarca888@gmail.com>
2023-03-12 17:19:11 -0700

    Render order button when cart has items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,6 +7,7 @@ const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -24,8 +25,10 @@ const Cart = (props) => {
         <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
-        <button className={classes["button"]}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes["button"]}>Order</button>}
       </div>
     </Modal>
   );
